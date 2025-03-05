9ed61c340d757a3bb667b7f60099bcc6605c2b9f
Ishaan Choubey <ishaanchoubey@Ishaans-MacBook-Pro.local>
2025-03-05 15:35:46 +0530

    Feature: Toggle light/dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/custom/footer";
 import Navbar from "@/components/custom/navbar";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 import { externals } from "@/constant/data";
 import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
@@ -9,18 +10,26 @@ import "./blog.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(externals.base_url),
   title: `${externals.name} . home`,
   description: externals.meta_description,
-  metadataBase: new URL(externals.base_url),
   openGraph: {
+    type: "website",
+    url: externals.base_url,
     title: `${externals.name} . home`,
     description: externals.meta_description,
-    url: `${externals.base_url}`,
-    images: ["/og.png"],
+    siteName: externals.name,
+    images: [
+      {
+        url: "/og.png",
+      },
+    ],
   },
   twitter: {
+    card: "summary_large_image",
     title: `${externals.name} . home`,
     description: externals.meta_description,
+    images: ["/og.png"],
   },
   alternates: {
     canonical: externals.base_url,
@@ -33,12 +42,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={cn(GeistSans.className, " max-w-2xl px-2 mx-auto")}>
-        <Navbar />
-        {children}
-        <Analytics mode="production" />
-        <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(GeistSans.className, "max-w-2xl px-2 mx-auto")}>
+        <ThemeProvider defaultTheme="dark">
+          <Navbar />
+          {children}
+          <Analytics mode="production" />
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -1,35 +1,35 @@
 import { externals } from "@/constant/data";
 import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
 
 export default function Navbar() {
-  // if (pathname.startsWith("/blog/")) return null;
-
   return (
     <nav className="py-3 flex items-center justify-between sticky top-0 bg-background">
       <Link
         href={"/"}
-        className="text-xl text-muted-foreground hover:text-white"
+        className="text-xl text-muted-foreground hover:text-foreground"
       >
         {externals.logo_text}
       </Link>
 
-      <ul className="flex gap-1 md:gap-2">
-        <li className="text-muted-foreground hover:text-white">
+      <ul className="flex items-center gap-1 md:gap-2">
+        <li className="text-muted-foreground hover:text-foreground">
           <Link className="flex items-center px-1" href={"/about"}>
             about
           </Link>
         </li>
-        <li className="text-muted-foreground hover:text-white">
+        <li className="text-muted-foreground hover:text-foreground">
           <Link className="flex items-center" href={"/blog"}>
             blog
           </Link>
         </li>
-        <li className="text-muted-foreground hover:text-white">
-        </li>
-        <li className="text-muted-foreground hover:text-white">
+        <li className="text-muted-foreground hover:text-foreground">
           <Link className="flex items-center" href={"/contact"}>
             contact
           </Link>
+        </li>
+        <li>
+          <ThemeToggle />
         </li>
       </ul>
     </nav>
