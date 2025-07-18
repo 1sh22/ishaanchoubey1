33f0b845ecfa53d16f3cb9e4535618ae6032dfb8
Ishaan Choubey <ishaanchoubey@Ishaans-MacBook-Pro.local>
2025-07-18 20:49:00 +0530

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,15 +28,7 @@ export default function Home() {
         <b className="font-normal text-secondary-foreground"> Bengaluru, India.</b> I work with code and sometimes write about things I&apos;m interested in (Mostly tech and startups)
       </p>
       <span className="mt-5 h-0 block" />
-      <p className="text-muted-foreground">
-        Final year student at
-        <b className="font-normal text-secondary-foreground">
-          <Link target="_blank" href={externals.compony.site}>
-            {" "}
-            {externals.compony.name}.{" "}
-          </Link>
-        </b>
-      </p>
+
       <span className="mt-12 h-0 block" />
       <h2 className="text-lg md:text-xl font-medium">Projects</h2>
       <span className="mt-5 h-0 block" />
